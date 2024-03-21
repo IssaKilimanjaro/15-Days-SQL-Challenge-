ae62338206ac8a10c0591348da2927b71f241f00
IssaKilimanjaro <100530755+IssaKilimanjaro@users.noreply.github.com>
2024-03-21 11:48:20 +0300

    Add files via upload

---
+++ day12.sql
@@ -0,0 +1,22 @@
+
+SELECT * 
+FROM clients;
+
+
+-- create CTE to select clients from Boxing sports
+WITH boxing_clients AS ( 
+	SELECT * 
+	FROM clients
+	WHERE sport = 'Boxing')
+SELECT * 
+FROM boxing_clients;
+
+
+-- create CTE to select clients that paying fee of more than 60k
+WITH fee60k_clients AS (
+	SELECT * 
+	FROM clients
+	WHERE fees > 60000.00
+)
+SELECT *
+FROM fee60k_clients;
